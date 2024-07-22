c531a67f314da644d0f92874954b26a43eaaee42
ezechiel <yaoezchiel@gmail.com>
2024-07-22 15:45:48 +0000

    interaction de sign-up et sign-in en js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.querySelector('.container');
+const ghostt = document.querySelector(".ghost");
 
+signin = document.querySelector('.signin')
+signup = document.querySelector('.signup')
+
+
+
+console.log(signup, signin)
 signUpButton.addEventListener('click', () => {
     container.classList.add('right-panel-active');
 });
@@ -10,3 +17,19 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
+
+ghostt.addEventListener("click", () => {
+    if (signup.classList.contains('view')){
+        signup.classList.remove('view')
+        signin.classList.add('view')
+    }else if(signin.classList.contains('view')){
+        signin.classList.remove('view')
+        signup.classList.add('view')
+    }
+
+})
+
+ghostt.addEventListener("click", ()=> {
+    console.log("pre");
+})
+
