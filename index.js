4dc7e0e608777e9373600122bdda32ccbee8dabf
ezechiel <yaoezchiel@gmail.com>
2024-07-28 16:51:38 +0000

    gestion des intéraction entre signup et signup

--- index.js
+++ index.js
@@ -2,14 +2,37 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.querySelector('.container');
-const ghostt = document.querySelector(".ghost");
 
-signin = document.querySelector('.signin')
-signup = document.querySelector('.signup')
+const signIn = document.querySelector('.signin');
+const signUp = document.querySelector('.signup');
+
+const ghostForm = document.querySelector(".ghost")
+
+// txt.addEventListener("click", () => {
+//     console.log('kkkk !');
+// })
 
 
 
-console.log(signup, signin)
+ghostForm.addEventListener("click", () => {
+
+    console.log("echo !");
+    if (signUp.classList.contains('view')){
+        signUp.classList.remove('view');
+        signIn.classList.add('view');
+    }else if(signIn.classList.contains('view')){
+        signIn.classList.remove('view');
+        signUp.classList.add('view');
+    }
+
+})
+// console.log(variable);
+// Select elements for 
+
+// console.log(signUp);
+
+
+
 signUpButton.addEventListener('click', () => {
     container.classList.add('right-panel-active');
 });
@@ -18,18 +41,5 @@ signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
 
-ghostt.addEventListener("click", () => {
-    if (signup.classList.contains('view')){
-        signup.classList.remove('view')
-        signin.classList.add('view')
-    }else if(signin.classList.contains('view')){
-        signin.classList.remove('view')
-        signup.classList.add('view')
-    }
 
-})
-
-ghostt.addEventListener("click", ()=> {
-    console.log("pre");
-})
 
